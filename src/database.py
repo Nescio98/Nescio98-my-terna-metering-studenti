b45a6ac8d336e54b00be4183745f211d8728c8e4
Lorenzo Astengo <lorenzo.astengo@ego.energy>
2022-05-05 16:27:50 +0200

    Adde dget parmeter method, policy and used it to get db host,user,pwd

--- src/database.py
+++ src/database.py
@@ -1,21 +1,26 @@
 import psycopg2
 import os
+from shared import *
 
-os.environ['db_hostname'] = "datalake-prod-eu-west-1b.cgeagqwpmv5k.eu-west-1.rds.amazonaws.com"
-os.environ['db_user'] = "datalake_admin"
-os.environ['db_password'] = "d0bDtpbjjyUrFycl"
+res = get_parameters(
+    [
+        "/prod/datalake/host",
+        "/prod/datalake/lambda/username",
+        "/prod/datalake/lambda/password",
+    ]
+)
 
 
 def get_db_connection():
     return psycopg2.connect(
-            database="datalake",
-            user=os.getenv('db_user'),
-            password=os.getenv('db_password'),
-            host=os.getenv('db_hostname'),
-            port=5432,
-            connect_timeout=3        
-            )
+        database="datalake",
+        user=os.getenv(res[1].Value),
+        password=os.getenv(res[2].Value),
+        host=os.getenv(res[0].Value),
+        port=5432,
+        connect_timeout=3,
+    )
+
+
 #     cursor = db.cursor()
 #     return db, cursor
-
-
